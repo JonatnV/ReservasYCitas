512e6cc2dfa90480b854392ef095765fb319aedd
JonatnV <116097178+JonatnV@users.noreply.github.com>
2024-03-13 19:44:29 -0500

    Creados Modelos, Services(Borradores)
    Se crearon los Modelos de los objetos necesarios para el sistema de cita, se crearon dos service que se usaran como base para la creación de los controller que harán las operaciones con Rest
    
    Co-Authored-By: nicorodri18 <100327214+nicorodri18@users.noreply.github.com>

--- src/main/java/com/negocio/ReservasYCitas/ReservasYCitasApplication.java
+++ src/main/java/com/negocio/ReservasYCitas/ReservasYCitasApplication.java
@@ -1,13 +1,22 @@
 package com.negocio.ReservasYCitas;
 
+import com.negocio.ReservasYCitas.Modelo.Menu;
+import com.negocio.ReservasYCitas.Service.CitaService;
+import com.negocio.ReservasYCitas.Service.FranjaHorariaService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.rmi.MarshalException;
 
 @SpringBootApplication
 public class ReservasYCitasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservasYCitasApplication.class, args);
+		FranjaHorariaService franjaHorariaService = new FranjaHorariaService();
+		CitaService citaService = new CitaService(franjaHorariaService);
+		Menu menu = new Menu(citaService, franjaHorariaService);
+		menu.ejecutar();
 	}
 
 }
